a027d70c56838fac70e145a1fcb060d592ff390c
shokiseta <109882210+shokiseta@users.noreply.github.com>
2022-07-24 11:34:02 +0900

    update

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("A")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("C")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("B")
+})
 radio.setGroup(1)
