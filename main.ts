c14467d240a030033a175f76b3e78ffa23fa539b
shokiseta <109882210+shokiseta@users.noreply.github.com>
2022-07-24 11:35:34 +0900

    文字列

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 input.onButtonPressed(Button.A, function () {
+    katakana.showString("マエ")
     radio.sendString("A")
 })
 input.onButtonPressed(Button.AB, function () {
+    katakana.showString("テイシ")
     radio.sendString("C")
 })
 input.onButtonPressed(Button.B, function () {
+    katakana.showString("ウシロ")
     radio.sendString("B")
 })
 radio.setGroup(1)
